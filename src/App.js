9d7a9c7b68c2cc24af51ac3cbbe95d824887c3dd
Suriya Prakash. S <84764163+SuriyaPrakash5@users.noreply.github.com>
2024-08-05 22:50:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header/Header";
+
+function App() {
+  return (
+    <div className="app">
+      {/* Header */}
+      <Header />
+
+      {/* App Body */}
+
+      {/* Sidebar */}
+
+      {/* Feed */}
+
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAkviCmDtVkUeUeBX5XRK93XaKi9LwEdYg",
+  authDomain: "linkedin-clone-ss-7a2d7.firebaseapp.com",
+  projectId: "linkedin-clone-ss-7a2d7",
+  storageBucket: "linkedin-clone-ss-7a2d7.appspot.com",
+  messagingSenderId: "755690538542",
+  appId: "1:755690538542:web:d32c4a9eb4c0064a2f81bf",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
